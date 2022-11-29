77d67f5aea17027de71a95a332b23d93e15d1422
최민한 <chmh0411@gmail.com>
2022-11-29 11:51:54 +0900

    fix: needle generate script

--- Project.swift
+++ Project.swift
@@ -35,6 +35,6 @@ let project = Project(
   targets: Project.makeAppTargets(
     name: "NeedleTest",
     dependencies: dependencies,
-    scripts: [.pre(script: "needle generate blblbl", name: "Needle Generate")]
+    scripts: [.pre(script: "needle generate Targets/NeedleTest/Sources/NeedleGenerated.swift Targets/NeedleTest/Sources/", name: "Needle Generate")]
   )
 )
